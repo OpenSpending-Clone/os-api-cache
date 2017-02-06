7114b7b37b0a107dd29e86499b0b1e8206767bc8
Adam Kariv <adam.kariv@gmail.com>
2017-02-06 17:36:31 +0200

    OS Specific cache

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='os-api-cache',
-    version='0.0.1',
+    version='0.0.2',
     description=(
         'Manage a cache for OpenSpending API'
     ),
